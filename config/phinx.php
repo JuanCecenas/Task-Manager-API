015ee2d864e09c9d9a7dc32aef0668e94b1c6ed7
Juan Cecenas <juancarlos.cecenasrojas@gmail.com>
2026-05-10 15:32:56 -0600

    Create Phinx config file and add Dotenv

---
+++ config/phinx.php
@@ -0,0 +1,23 @@
+<?php
+
+return
+[
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'default' => [
+            'adapter' => 'mysql',
+            'host' => 'localhost',
+            'name' => 'task_manager',
+            'user' => 'root',
+            'pass' => '',
+            'port' => '3306',
+            'charset' => 'utf8',
+        ]
+    ],
+    'version_order' => 'creation'
+];
